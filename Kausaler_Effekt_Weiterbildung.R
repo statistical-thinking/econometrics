e73fa7f3fb9304afa72efedd20a33a732b39658d
Dr. Dennis Klinkhammer <dennis.klinkhammer@outlook.com>
2020-06-26 21:30:22 +0200

    Update Kausaler_Effekt_Weiterbildung.R

--- Kausaler_Effekt_Weiterbildung.R
+++ Kausaler_Effekt_Weiterbildung.R
@@ -3,7 +3,7 @@
 ###################
 
 # Datensatz von Server laden
-RDD <- read.csv("http://www.statistical-thinking.de/r-daten/RDD.csv", head=T, sep=";", dec=".")
+RDD <- read.csv("http://www.statistical-thinking.de/r-daten/RDD.csv", head=T, sep=";", dec=".", fileEncoding="UTF-8-BOM")
 head(RDD)
 
 # Grafische Darstellung der Diskontinuität
